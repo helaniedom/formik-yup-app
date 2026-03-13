e34ce6752199507fae62ca5a389616ec0b8901fe
helaniedom <helanied18@gmail.com>
2026-03-13 13:45:16 -0600

    edited app nav order of code

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,18 +1,18 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
 import SignInScreen from "../screens/SignInScreen";
 import SignUpScreen from "../screens/SignUpScreen";
-import HomeScreen from "../screens/HomeScreen";
 import EmployeeFormScreen from "../screens/EmployeeFormScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
     return (
-        <Stack.Navigator initialRouteName="SignIn">
-        <Stack.Screen name="SignIn" component={SignInScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="EmployeeForm" component={EmployeeFormScreen} />
+        <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="SignIn" component={SignInScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
+            <Stack.Screen name="EmployeeForm" component={EmployeeFormScreen} />
         </Stack.Navigator>
     );
 }
