b5f297b18edeaa5bf41e1687987be19f9db33c5b
cresdom <domcres807@icloud.com>
2026-03-12 19:56:46 -0700

    SignUpScreen initial creation

---
+++ screens/SignUpScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function SignUpScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Sign Up</Text>
+            <Text style={styles.subtitle}>Sign up form here yuhhhh.</Text>
+        </View>
+    );
+}
